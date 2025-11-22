4014704bcbca0c108288cb50c417cab8af64de90
gerson <gersonsennaa@gmail.com>
2025-11-22 11:11:55 -0300

    atualizando a estrutura develop-gerson

--- praticaFinal/routes/usuariosRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { cadastrar, login } = require('../controllers/usuariosController');
-
-router.post('/cadastrar', cadastrar);
-router.post('/login', login);
-
-module.exports = router;
